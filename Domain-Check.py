534accc2adc7fc239fa039a0c01187aa145ea979
papu-rex <adolfoherreraayala666@gmail.com>
2025-10-25 18:28:57 +0000

    agregue el codigo xd

---
+++ Domain-Check.py
@@ -0,0 +1,15 @@
+dominio = input("dominio:")
+d1 = "service@paypal.com.mx"
+d2 = "info@mercadopago.com"
+d3 = "no-responder@mercadolibre.com"
+d4 = "noreply-purchases@youtube.com"
+d5 = "noreply@steampowered.com"
+d6 = "info@mail.fondeadora.app"
+d7 = "noreply@didiglobal.com"
+d8 = "noreply@accounts.google.com"
+d9 = "googleplay-noreply@google.com"
+d10 = "notifications@discord.com"
+if dominio in [d1,d2,d3,d4,d5,d6,d7,d8,d9,d10]:
+    print("dominio seguro")
+else:
+    print("fuchi kk")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # domain_check
 filtra dominios seguros comparando el dominio ingresado con el de una lista predefinida
+pueden agregar mas dominios al codigo para aumentar la precision
